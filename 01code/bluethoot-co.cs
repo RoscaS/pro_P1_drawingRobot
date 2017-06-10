27826bfc0c3144f634010d15125d3fe56424f327
RoscaS <sol.rosca@he-arc.ch>
2017-06-10 22:55:30 +0200

    dev

--- 01code/bluethoot-co.cs
+++ 01code/bluethoot-co.cs
@@ -22,8 +22,11 @@ private void btnBluetooth_Co(object sender, EventArgs e)
                           BluetoothService.SerialPort);
 
             try {
+                Cursor.Current = Cursors.WaitCursor;
+
                 client.Connect(ep);
                 clientConnected = true;
+
                 if (clientConnected && fileLoaded) {
                     btnSendScara.Enabled = true;
                 }
@@ -37,79 +40,5 @@ private void btnBluetooth_Co(object sender, EventArgs e)
         else {
             MessageBox.Show("Device not paired", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// plan
-
-private void btnBluetooth_Co(object sender, EventArgs e)
-{ // connexion
-
-    // pairing
-
-    // options
-
-    if (sbdd.ShowDialog() == DialogResult.OK)
-    {        
-        if (sbdd.SelectedDevice.Authenticated)
-        { //connecter perif, doit être pairer avant
-            Debug.WriteLine("Authenticated");
-
-            bluetoothAddress = 
-                sbdd.SelectedDevice.DeviceAddress;
-
-            client = new BluetoothClient();
-
-            BluetoothEndPoint ep = 
-                new BluetoothEndPoint(bluetoothAddress, 
-                          BluetoothService.SerialPort);
-            
-            try
-            {
-                Cursor.Current = Cursors.WaitCursor;
-
-                client.Connect(ep);
-                clientConnected = true;
-                if (clientConnected && fileLoaded)
-                {
-                    btnSendScara.Enabled = true;
-                }
-                Debug.WriteLine("Connected");
-
-                Cursor.Current = Cursors.Arrow;
-                btnBluetooth.ForeColor = Color.Green;
-
-            }
-            catch
-            {
-
-                Cursor.Current = Cursors.Arrow;
-
-                MessageBox.Show("Device not *LISTENING*", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
-
-            //client.Close();
-
-        }
-        else
-        {
-            MessageBox.Show("Device not paired", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
-
     }
 }
